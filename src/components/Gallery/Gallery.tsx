13b790ce9d8dec4ae38ece02dbe35c6a352d8860
Luke Kim <burgund32@gmail.com>
2022-04-28 12:46:30 +0900

    feat : 사진 후원 오카방 링크 클릭시 새로운 윈도우 Open

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -10,7 +10,12 @@ const Gallery = (): JSX.Element => {
       <div className="carousel-image-container">
         <CarouselImage items={CAROUSEL_IMAGE_LIST} />
       </div>
-      <a className="open_kakao_link" href="https://open.kakao.com/o/gYjxcqde">
+      <a
+        className="open_kakao_link"
+        target="_blank"
+        rel="noreferrer"
+        href="https://open.kakao.com/o/gYjxcqde"
+      >
         <span className="open_kakao_link_icon">👉</span> 사진 후원 오카방 바로가기
       </a>
     </div>
