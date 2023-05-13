703d528ee3cc67a90d831509be347b5755488554
최규민[Maps Service FE] <gyumin.choi@navercorp.com>
2023-05-13 16:48:45 +0900

    update main color to blue

--- src/components/OpenParagraph/OpenParagraph.tsx
+++ src/components/OpenParagraph/OpenParagraph.tsx
@@ -5,7 +5,9 @@ const OpenParagraph = (): JSX.Element => {
   return (
     <div className="open_paragraph">
       <h2 className="open_paragraph_title">모시는 글</h2>
-      <span className="open_paragraph_text">태어난지 9861일 되는 따뜻한 봄날</span>
+      <span className="open_paragraph_text">
+        태어난지 <strong className="strong">9861일</strong> 되는 따뜻한 봄날
+      </span>
       <span className="open_paragraph_text">🎉신나는 파티👯‍♂️ 마 한번 쌔릴까 합니다</span>
       <span className="open_paragraph_text">부디 귀한 발걸음 하시어 💃즐겨🕺주시면</span>
       <span className="open_paragraph_text">더없는 기쁨으로 간직하겠습니다</span>
